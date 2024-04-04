7097675fa6589fcb7d5587738011e73f10af2415
Collin Delker <44102190+cdelker@users.noreply.github.com>
2024-04-04 15:34:15 -0600

    Add more functions to risk calculator. Remove suncal dependency from tur-based risk calculator for faster startup.

--- suncal/src/risk.py
+++ suncal/src/risk.py
@@ -1,10 +1,14 @@
 ''' Measurement Decision Risk Calculator - pyscript interface '''
 from pyscript import document, display
+import math
 
 from suncal.risk.risk_model import RiskModel
 from suncal.common import distributions
+from suncal.common import plotting
 from suncal.common.report import Report
 
+plotting.plotstyle['figure.figsize'] = (6, 4)
+plotting.plotstyle['font.size'] = 12
 Report.apply_css = False  # Use this page's CSS, not Suncal's built-in CSS
 
 
@@ -13,10 +17,17 @@ def get_model():
     proc_median = float(document.getElementById("Median").value)
     meas_median = float(document.getElementById("bias").value)
     measurement = float(document.getElementById("measurement").value)
-    lower_lim = float(document.getElementById("LowL").value)
-    upper_lim = float(document.getElementById("UpL").value)
     proc_dist_name = document.getElementById("procdist").textContent
     meas_dist_name = document.getElementById("measdist").textContent
+    if document.getElementById("LowLimitEnable").checked:
+        lower_lim = float(document.getElementById("LowL").value)
+    else:
+        lower_lim = -math.inf
+
+    if document.getElementById("UpLimitEnable").checked:
+        upper_lim = float(document.getElementById("UpL").value)
+    else:
+        upper_lim = math.inf
 
     if proc_dist_name == 'normal':
         procargs = {
@@ -71,18 +82,49 @@ def get_model():
 def calculate(event=None):
     ''' Update full risk calculation '''
     model = get_model()
+    if not math.isfinite(model.speclimits[0]) and not math.isfinite(model.speclimits[1]):
+        display('Risk calculation must have at least one specification limit enabled',
+                target='output', append=False)
+        display('', target='plot', append=False)
+        return
+
+    conditional = document.getElementById("conditional").checked
+
     result = model.calculate()
-    display(result.report.summary(), target='output', append=False)
+    display(result.report.summary(conditional=conditional), target='output', append=False)
     display(result.report.plot.joint(), target='plot', append=False)
 
 
 def calc_guardband(event=None):
     ''' Calculate guardband '''
-    pfa = float(document.getElementById("TargetPFA").value) / 100
     model = get_model()
-    model.guardband_pfa(pfa=pfa)
+    gbmode = event.target.id
+    if gbmode in ['rss', 'dobbert', 'rp10', '4:1', 'test']:
+        model.guardband_tur(gbmode)
+    elif gbmode in ['pfa']:
+        target = float(document.getElementById("TargetPFA").value) / 100
+        conditional = document.getElementById("conditional").checked
+        model.guardband_pfa(pfa=target, conditional=conditional)
+    elif gbmode in ['pfr']:
+        target = float(document.getElementById("TargetPFR").value) / 100
+        model.guardband_pfr(pfr=target)
+    elif gbmode in ['specific']:
+        target = float(document.getElementById("worstspecific").value) / 100
+        model.guardband_specific(target=target)
+
+    document.getElementById("guardband").checked = True
     document.getElementById("lowerGuard").value = model.gbofsts[0]
     document.getElementById("upperGuard").value = model.gbofsts[1]
+    enable_guardband()
+    calculate()
+
+
+def update_limit_enable(event=None):
+    ''' Lower/Upper limit checkbox changed '''
+    low_on = document.getElementById("LowLimitEnable").checked
+    hi_on = document.getElementById("UpLimitEnable").checked
+    document.getElementById("LowL").disabled = not low_on
+    document.getElementById("UpL").disabled = not hi_on
     calculate()
 
 
@@ -140,18 +182,14 @@ def update_measure_rows(event):
     calculate()
 
 
-def enable_guardband(event):
+def enable_guardband(event=None):
     ''' Guardband checkbox clicked '''
     if document.getElementById("guardband").checked:
         document.getElementById("lowerGuard").disabled = False
         document.getElementById("upperGuard").disabled = False
-        document.getElementById("TargetPFA").disabled = False
-        document.getElementById("calculateGBF").disabled = False
     else:
         document.getElementById("lowerGuard").disabled = True
         document.getElementById("upperGuard").disabled = True
-        document.getElementById("TargetPFA").disabled = True
-        document.getElementById("calculateGBF").disabled = True
     calculate()
 
 
--- suncal/src/risktur.py
+++ suncal/src/risktur.py
@@ -1,20 +1,94 @@
-''' Simple Risk Calculator pyscript interface '''
+''' Simple Risk Calculator pyscript interface
+
+    For efficiency, does not load Suncal. Uses standalone, no-dependency
+    risk calculation using only python's builtin statistics library.
+    A tad slower to calculate, but so much faster to initialize.
+'''
 from pyscript import document, display
 
-from suncal.risk.risk_model import RiskModelSimple
-from suncal.common.report import Report
+import statistics
 
-Report.apply_css = False  # Use this page's CSS, not Suncal's built-in CSS
+standard_normal = statistics.NormalDist()
 
 
 def risk_simple(event=None):
-    ''' Update simple risk calculation '''
+    ''' Update simple risk calculation from controls on page '''
     tur = float(document.getElementById('TUR').value)
     itp = float(document.getElementById('ITP').value) / 100
     gbf = float(document.getElementById('GBF').value)
-    model = RiskModelSimple(tur=tur, itp=itp, gbf=gbf)
-    result = model.calculate()
-    display(result.report.summary(n=3), target='output', append=False)
+    model = RiskResult(itp, tur, gbf)
+    display(model, target='output', append=False)
+
+
+class RiskResult:
+    ''' Simple Risk Calculator '''
+    def __init__(self, itp: float = .95, tur: float = 4, gbf: float = 1):
+        self.tur = tur
+        self.itp = itp
+        self.gbf = gbf
+        self.T = 1  # Normalize to a tolerance of +/- 1
+
+        self.sigma_p = 1 / standard_normal.inv_cdf((1+self.itp)/2)
+        self.sigma_m = 1/self.tur/2
+        self.accept_limit = self.T*self.gbf
+
+    def pfa(self, N=10000):
+        ''' Calculate global PFA '''
+        def pfa_integrand(z):
+            phi_z = standard_normal.pdf(z)
+            f_z = standard_normal.cdf((self.accept_limit-self.sigma_p*z)/self.sigma_m) - standard_normal.cdf((-self.accept_limit-self.sigma_p*z)/self.sigma_m)
+            return phi_z * f_z
+
+        inf = max(2, self.sigma_p*6)  # "infinity" for the integration
+        dz = (inf-1)/N
+        zvalues = [self.T/self.sigma_p + i*dz for i in range(N)]
+        pfa = sum(map(pfa_integrand, zvalues[1:-1])) + (pfa_integrand(zvalues[0]) + pfa_integrand(zvalues[-1]))/2
+        pfa = 2 * pfa * dz
+        return pfa
+
+    def pfr(self, N=10000):
+        ''' Calculate global PFR '''
+        def pfr_integrand(z):
+            phi_z = standard_normal.pdf(z)
+            f_z = standard_normal.cdf((self.accept_limit-self.sigma_p*z)/self.sigma_m) - standard_normal.cdf((-self.accept_limit-self.sigma_p*z)/self.sigma_m)
+            return phi_z * (1-f_z)
+
+        dz = 2*self.T/self.sigma_p/N
+        zvalues = [-self.T/self.sigma_p + i*dz for i in range(N)]
+        pfr = sum(map(pfr_integrand, zvalues[1:-1])) + (pfr_integrand(zvalues[0]) + pfr_integrand(zvalues[-1]))/2
+        pfr *= dz
+        return pfr
+
+    def worst_specific(self):
+        ''' Worst-case specific risk '''
+        measuredist = statistics.NormalDist(self.accept_limit, self.sigma_m)
+        risk_lower = measuredist.cdf(-self.T)
+        risk_upper = 1-measuredist.cdf(self.T)
+        return risk_lower + risk_upper
+
+    def process_risk(self):
+        ''' Process risk '''
+        risk_total = 1-self.itp
+        return risk_total
+
+    def _repr_html_(self):
+        ''' HTML representation for display function '''
+        pfa = self.pfa()
+        pfr = self.pfr()
+        wspecific = self.worst_specific()
+        proc = self.process_risk()
+        return f'''<table>
+        <thead><tr>
+            <td>Process Risk</td>
+            <td>Specific Measurement Risk</td>
+            <td>Global Risk</td>
+        </tr></thead>
+        <tbody>
+        <tr><td>Process Risk: {proc*100:.2f}%</td><td>TUR: {self.tur:.1f}</td><td>Total PFA: {pfa*100:.3f}%</td></tr>
+        <tr><td>Upper Limit Risk: {proc*100/2:.2f}%</td><td>Worst-case Specific Risk: {wspecific*100:.1f}%</td><td>Total PFR: {pfr*100:.2f}%</td></tr>
+        <tr><td>Lower Limit Risk: {proc*100/2:.2f}%</td><td></td><td></td></tr>
+        </tbody>
+       </table>'''
 
 
 # Initialize
